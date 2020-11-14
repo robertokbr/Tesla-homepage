5a0e8bb37c0cbf40674a35d8645668ab0c0babab
robertokbr <robertojuniordev@gmail.com>
2020-11-13 21:07:57 -0300

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/Home';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <Home />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
