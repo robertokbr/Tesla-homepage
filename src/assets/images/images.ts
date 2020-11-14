fe624648d5ce8c8c08f6376cf3a2bece1956676b
robertokbr <robertojuniordev@gmail.com>
2020-11-13 21:22:07 -0300

    Add background assets

---
+++ src/assets/images/images.ts
@@ -0,0 +1,6 @@
+export { default as car1 } from './car-1.jpg';
+export { default as car2 } from './car-2.jpg';
+export { default as car3 } from './car-3.jpg';
+export { default as car4 } from './car-4.jpg';
+export { default as car5 } from './car-5.jpg';
+export { default as car6 } from './car-6.jpg';
