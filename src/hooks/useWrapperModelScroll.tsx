45ac98c0549b6b3953dd2c26a484ff32ef3e25f3
robertokbr <robertojuniordev@gmail.com>
2021-02-13 22:56:46 -0300

    Add useWrapperModelSroll hook

---
+++ src/hooks/useWrapperModelScroll.tsx
@@ -0,0 +1,35 @@
+import { useMotionValue } from 'framer-motion';
+import { useContext, useEffect } from 'react';
+import { ModelContext } from './useModel';
+
+export default function useWrapperScroll() {
+  const { wrapperRef } = useContext(ModelContext);
+
+  const scrollY = useMotionValue(0);
+  const scrollYProgress = useMotionValue(0);
+
+  useEffect(() => {
+    const element = wrapperRef.current;
+
+    if (element) {
+      const updateScrollValue = () => {
+        const { scrollTop, scrollHeight, offsetHeight } = element;
+
+        const fullScroll = scrollHeight - offsetHeight;
+
+        scrollY.set(scrollTop);
+
+        scrollYProgress.set(scrollTop / fullScroll);
+      };
+
+      element.addEventListener('scroll', updateScrollValue);
+
+      return () => element.removeEventListener('scroll', updateScrollValue);
+    }
+  }, [scrollY, scrollYProgress, wrapperRef]);
+
+  return {
+    scrollY,
+    scrollYProgress,
+  };
+}
