41e4482703ae75e7d92057fca51e4b2fa8b6b703
robertokbr <robertojuniordev@gmail.com>
2021-02-13 22:57:29 -0300

    Update  DefaultOverlayContent

--- src/components/DefaultOverlayContent/index.tsx
+++ src/components/DefaultOverlayContent/index.tsx
@@ -4,19 +4,24 @@ import * as S from './styles';
 
 interface OverlayProps {
   label: string;
-  description: string;
 }
 
-const DefaultOverlayContent: React.FC<OverlayProps> = ({
-  description,
-  label,
-}) => {
+const DefaultOverlayContent: React.FC<OverlayProps> = ({ label }) => {
   return (
     <S.Container>
       <S.Heading>
         <h1>{label}</h1>
-        <h2>{description}</h2>
+        <h2>
+          Order Online for
+          <span>Touchless Delivery</span>
+        </h2>
       </S.Heading>
+      <S.Buttons>
+        <button type="button">Custom order</button>
+        <button type="button" className="white">
+          Existing inventory
+        </button>
+      </S.Buttons>
     </S.Container>
   );
 };
--- src/components/DefaultOverlayContent/styles.ts
+++ src/components/DefaultOverlayContent/styles.ts
@@ -1,5 +1,81 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+interface ButtonProps {
+  white?: number;
+}
 
-export const Heading = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Heading = styled.div`
+  margin-top: 16.5vh;
+  width: 100%;
+  text-align: center;
+
+  > h1 {
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 50px;
+    color: #393c41;
+  }
+
+  > h2 {
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 20px;
+    color: #5c5e62;
+
+    span {
+      border-bottom: 1px solid #393c41;
+      color: #000;
+    }
+  }
+`;
+
+export const Buttons = styled.div<ButtonProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 130px;
+  text-transform: uppercase;
+
+  button {
+    background: #1a1720;
+    color: #fff;
+    opacity: 0.8;
+    font-size: 12px;
+    font-weight: 500;
+    letter-spacing: 0.4px;
+    text-transform: uppercase;
+    border-radius: 20px;
+    border: none;
+    outline: 0;
+    cursor: pointer;
+    padding: 13px 40px;
+
+    > & + button {
+      margin: 0 0 20px;
+    }
+
+    &.white {
+      background: #fff;
+      color: #1a1720;
+      opacity: 0.65;
+    }
+  }
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+    margin: 0 0 90px;
+
+    > button + button {
+      margin: 0 0 0 10px;
+    }
+  }
+`;
